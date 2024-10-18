59f11665e89d5f00f90e518179ab742fad274131
4eDo <61490588+4eDo@users.noreply.github.com>
2024-10-18 14:30:26 +0300

    Update fillTemplatesList.js

--- forums/templates/fillTemplatesList.js
+++ forums/templates/fillTemplatesList.js
@@ -136,8 +136,8 @@ function drawForm(id) {
         let label = document.createElement('label');
         label.innerText = field.name;
         let labelDescr = document.createElement('div');
-        labelDescr.innerText = field.info.replace("{{LINK_TEMPLATE}}", `<a href='ссылка на профиль'>ваш любимчик</a>`);
-        labelDescr.innerText = field.info.replace("<br>", `\n`);
+        labelDescr.innerText = field.info.replaceAll("{{LINK_TEMPLATE}}", `<a href='ссылка на профиль'>ваш любимчик</a>`);
+        labelDescr.innerText = field.info.replaceAll("<br>", `\n\n`);
         labelCell.appendChild(label);
         labelCell.appendChild(labelDescr);
 
